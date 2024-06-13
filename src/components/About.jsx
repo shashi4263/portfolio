23fc99f409692f0114564e952ae3350809508655
shashikiller <rshashi133@gmail.com>
2024-06-13 23:28:53 +0530

    index.css and Abou boundaryt updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const ServiceCard = ({ index, title, icon }) => {
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className="w-full green-piunk-gradient p-[1px] rounded-[20px] shadow-card"
+        className="card-container p-[1px] rounded-[20px] shadow-card"
       >
         <div
           options={{
